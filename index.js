8af194870cb239331a9b20861e8e6e67d4f6d9d2
cmuth001 <cmuth001@odu.edu>
2019-02-22 22:33:17 -0500

    removed unwanted stuff from index.js

--- index.js
+++ index.js
@@ -1,58 +1,20 @@
-var app = require('express')();
+var Employee = require('./model/services.js');
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var request = require('request');
+app.use('/public', express.static(__dirname + '/public'));
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/views/index.html');
 });
 
-var request = require("request");
-var cheerio = require("cheerio");
-var totalEmployees = [];
-request({
-  uri: "https://www.pdq.com/about-us/",
-}, function(error, response, body) {
-  var $ = cheerio.load(body);
-  $(".imghvr-fade> img").each(function() {
-    var link = $(this);
-    var image = link.attr("src");
-    var username = link.attr("alt");
-    totalEmployees.push({"username":username, 'image':image});
-  });
-});
 
 
 io.on('connection', function(socket){
   socket.on('getEmployee', function(){
-    request({
-        url: "https://pdqemployees.azurewebsites.net/api/pdqemployees",
-        json: true
-    }, function (error, response, body) {
-    
-        if (!error && response.statusCode === 200) {
-          console.log("UserName:"+body.name);
-          console.log("Drink:"+body.drink);
-        totalEmployees.forEach(function(item) {
-            if (body.name ==item.username){
-                request({
-                    url: item.image,
-                }, function (error, response, bodyImage) {
-                    if (error != "Invalid URI") {
-                        io.emit('getEmployee', {"name":body.name, "drink": body.drink, 'image': item.image});
-                    }else{
-                        io.emit('getEmployee', {"name":body.name, "drink": body.drink, 'image': "//cdn.pdq.com/wp-content/uploads/2017/10/default.png"});
-                    }
-                    
-                });
-                
-            }
-        
-        });
-        
-
-        }else{
-            console.log("resposeCode:"+response.statusCode)
-        }
+    var empObj = new Employee();
+    empObj.getEmployeeImage(function(data){
+      io.emit('getEmployee', data);
     });
     
   });
